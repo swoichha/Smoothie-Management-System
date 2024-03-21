d2a350a2dd42bc8cf5c0b9e80a97f69a8f50c0ec
swoichha adhikari <swoichha@swoichhas-MacBook-Pro.local>
2024-03-21 19:46:45 +0545

    created table in supabase and fetching data

--- supa-smoothies/src/pages/Home.js
+++ supa-smoothies/src/pages/Home.js
@@ -1,12 +1,44 @@
 import supabase from "../config/supabaseClient"
+import { useEffect, useState } from "react"
 
 const Home = () => {
-  console.log(supabase)
-    return (
-      <div className="page home">
-        <h2>Home</h2>
-      </div>
-    )
-  }
+  const [fetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  //FETCH DATA FROM SMOOTHIES TABLE
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from('smoothies')
+        .select()
+      
+      if (error) {
+        setFetchError("Could not fetch the smoothies")
+        setSmoothies(null)
+        console.log(error)
+      }
+
+      if (data) {
+        setSmoothies(data)
+        setFetchError(null)
+      }
+    }
+
+    fetchSmoothies()
+  }, [])
+    
+  return (
+    <div className="page home">
+      {fetchError && (<p>{fetchError}</p>)}
+      {smoothies && (
+        <div className="smoothiesCard">
+          {smoothies.map(smoothie => (
+            <p>{smoothie.title}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
   
   export default Home
