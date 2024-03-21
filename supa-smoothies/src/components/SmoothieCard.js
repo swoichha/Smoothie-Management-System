73f9ecd24e516820c3aa45dd5ef7e61ee9a855a2
swoichha adhikari <swoichha@swoichhas-MacBook-Pro.local>
2024-03-21 22:15:24 +0545

    deleting record

--- supa-smoothies/src/components/SmoothieCard.js
+++ supa-smoothies/src/components/SmoothieCard.js
@@ -1,15 +1,34 @@
 import { Link } from "react-router-dom"
+import supabase from "../config/supabaseClient"
 
-const SmoothieCard = ({smoothie}) => {
+
+const SmoothieCard = ({ smoothie }) => {
+  
+  const handleDelete = async () => {
+    const { data, error } = await supabase
+      .from('smoothies')
+      .delete()
+      .eq('id', smoothie.id)    
+      .select()
+    
+    if (error) {
+      console.log(error)
+    }
+    if (data) {
+      console.log(data)
+    }
+  }
+
   return (
     <div className="smoothie-card">
-          <h3>{smoothie.title}</h3>
-          <p>{smoothie.method}</p>
-          <div className="rating">{smoothie.rating}</div>
+        <h3>{smoothie.title}</h3>
+        <p>{smoothie.method}</p>
+        <div className="rating">{smoothie.rating}</div>  
           <div className="update-btn">
-              <Link to={'/' + smoothie.id}>
-                  <i className="material-icons">edit</i>
-              </Link>
+            <Link to={'/' + smoothie.id}>
+              <i className="material-icons">edit</i>
+            </Link>
+            <i className="material-icons" onClick={handleDelete}>delete</i>
           </div>
     </div>
   )
